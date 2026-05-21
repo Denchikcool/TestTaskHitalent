403bc4360cfed9209924e95387963bc88a9ef16b
Denchikcool <kuzmenok.den@yandex.ru>
2026-05-21 14:47:35 +0700

    Добавлена переадресация на Swagger

--- app/main.py
+++ app/main.py
@@ -8,6 +8,11 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(
 app = FastAPI(title="Org Structure API", version="1.0.0")
 app.include_router(department_router, prefix="/api/v1")
 
+@app.get("/", include_in_schema=False)
+async def root():
+    from fastapi.responses import RedirectResponse
+    return RedirectResponse(url="/docs")
+
 @app.get("/health", tags=["health"])
 async def health():
     return {"status", "ok"}
